8d51720a0071cd6d0295bb76dc8c4fab6db0e5a4
Anik Siddiky <codesbynik@gmail.com>
2025-06-29 04:56:37 +0600

    fixed the nav drawer for mobile and added a favicon

--- src/Components/FancyButton.jsx
+++ src/Components/FancyButton.jsx
@@ -4,11 +4,10 @@ const FancyButton = ({ children, onClick, className = "" }) => {
   return (
     <button
       onClick={onClick}
-      className={`hover:scale-105 transition relative inline-flex items-center justify-center px-8 py-2.5 overflow-hidden text-white bg-[#0E4632] rounded-sm group cursor-pointer ${className}`}
+      className={`hover:scale-105 transition relative inline-flex items-center justify-center px-8 py-2.5 overflow-hidden text-white bg-[#A87914] rounded-sm group cursor-pointer ${className}`}
     >
-      <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#093324] rounded-full group-hover:w-56 group-hover:h-56"></span>
-      <span className="absolute inset-0 w-full h-full -mt-1 rounded-lg opacity-30 bg-gradient-to-b from-transparent via-transparent to-gray-700"></span>
-      <span className="relative">{children}</span>
+      <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#705211] rounded-full group-hover:w-56 group-hover:h-56"></span>
+      <span className="relative z-10">{children}</span>
     </button>
   );
 };
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,38 +4,86 @@ import { FaSquareXTwitter } from 'react-icons/fa6';
 import { Link } from 'react-router';
 import anik from '../assets/aniksiddiky.png'
 import FancyButton from './FancyButton';
+import { Typewriter } from 'react-simple-typewriter';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
+
     return (
         <div className='lg:py-36 py-10 lg:flex items-center'>
-
             <div className='lg:space-y-5 space-y-3 flex-1'>
-                <h1 className='lg:text-7xl text-4xl font-bold text-center lg:text-start'>Anik Siddiky</h1>
-                <h3 className='text-xl lg:text-3xl font-medium text-center lg:text-start text-gray-500'>Front-End Developer</h3>
-                <p className='text-[17px] text-center lg:text-start'>Innovative Front-End Developer crafting smooth, accessible, and visually appealing web experiences. Passionate about writing clean code that creates intuitive and engaging interfaces.</p>
+                <motion.h1
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8, ease: 'easeOut' }}
+                    className='lg:text-7xl text-4xl font-bold text-center lg:text-start'>
+                    Anik Siddiky
+                </motion.h1>
+
+                <motion.h3
+                    initial={{ opacity: 0, y: -50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.8, ease: 'easeOut', delay: 0.7 }}
+                    className='text-xl lg:text-3xl font-medium text-center lg:text-start text-gray-500'>
+                    Front-End Developer
+                </motion.h3>
+
+                <p className='lg:text-xl text-center lg:text-start'>
+                    Innovative Front-End Developer crafting smooth, accessible, and visually appealing web experiences. Passionate about writing clean code that
+                    <br />
+                    <span className='text-[#A87914] font-medium'>
+                        <Typewriter
+                            words={[
+                                'creates intuitive and engaging interfaces!',
+                                'elevates user experience across all devices!',
+                                'transforms ideas into interactive designs!',
+                                'blends performance with aesthetic precision!',
+                            ]}
+                            loop={true}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={50}
+                            deleteSpeed={30}
+                            delaySpeed={1500} />
+                    </span>
+                </p>
+
                 <div className="flex gap-4 items-center justify-center lg:justify-start">
-                    <Link to="https://github.com/anik-siddiky" target="_blank" className="hover:text-[#0E4632] transition text-3xl sm:text-4xl">
-                        <FaGithub />
-                    </Link>
-                    <Link to="https://www.linkedin.com/in/anik-siddiky" target="_blank" className="hover:text-[#0E4632] transition text-3xl sm:text-4xl">
-                        <FaLinkedin />
-                    </Link>
-                    <Link to="https://x.com/codesbynik" target="_blank" className="hover:text-[#0E4632] transition text-3xl sm:text-4xl">
-                        <FaSquareXTwitter />
-                    </Link>
+                    <motion.div
+                        animate={{ scale: [1, 1.2, 1] }}
+                        transition={{ duration: 2.5, repeat: Infinity, ease: 'easeInOut' }}>
+                        <Link to="https://github.com/anik-siddiky" target="_blank" className="hover:text-[#A87914] transition text-3xl sm:text-4xl">
+                            <FaGithub />
+                        </Link>
+                    </motion.div>
+
+                    <motion.div
+                        animate={{ scale: [1, 1.2, 1] }}
+                        transition={{ duration: 2.5, repeat: Infinity, ease: 'easeInOut' }}>
+                        <Link to="https://www.linkedin.com/in/anik-siddiky" target="_blank" className="hover:text-[#A87914] transition text-3xl sm:text-4xl">
+                            <FaLinkedin />
+                        </Link>
+                    </motion.div>
+
+                    <motion.div
+                        animate={{ scale: [1, 1.2, 1] }}
+                        transition={{ duration: 2.5, repeat: Infinity, ease: 'easeInOut' }}>
+                        <Link to="https://x.com/codesbynik" target="_blank" className="hover:text-[#A87914] transition text-3xl sm:text-4xl">
+                            <FaSquareXTwitter />
+                        </Link>
+                    </motion.div>
                 </div>
 
-                <div className='text-center lg:text-start'>
+
+                <div className='text-center lg:text-start z-0'>
                     <Link to='/'>
-                        <FancyButton className='px-8 text-xl'>Resume</FancyButton>
+                        <FancyButton className='px-8 lg:text-xl mt-2 lg:mt-0'>Resume</FancyButton>
                     </Link>
                 </div>
             </div>
 
-
-
             <div className='flex-1 flex justify-center lg:justify-end lg:mt-0 mt-8'>
-                <img className='rounded-full w-72 lg:w-[550px]' src={anik} alt="" />
+                <img className='rounded-full w-72 lg:w-[500px]' src={anik} alt="" />
             </div>
         </div>
     );
