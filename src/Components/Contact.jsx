08c438a1a932c0338361ace1cbc1dfe1ccf6ff65
Anik Siddiky <codesbynik@gmail.com>
2025-06-30 08:50:29 +0600

    added font

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -46,7 +46,7 @@ const Contact = () => {
                         <label className="block mb-1 font-medium">Name</label>
                         <input
                             type="text"
-                            className="w-full border  dark:border-gray-200 px-4 py-2 rounded bg-gray-200 dark:bg-base-200 focus:outline-gray-300 focus:ring-2 focus:ring-black"
+                            className="w-full border dark:border-gray-200 px-4 py-2 rounded bg-gray-200 dark:bg-base-200 focus:outline-gray-300 focus:ring-2 focus:ring-black"
                             placeholder="Your Name"
                         />
                     </motion.div>
