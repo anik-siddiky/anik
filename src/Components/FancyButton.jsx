74ee33a1c3ceb4269132eb7f522e42a6fc71646b
Anik Siddiky <codesbynik@gmail.com>
2025-07-01 19:45:02 +0600

    fixed the vercel dual issue

--- src/Components/FancyButton.jsx
+++ src/Components/FancyButton.jsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const FancyButton = ({ children, onClick, className = "" }) => {
   return (
-    <button
-      onClick={onClick}
+    <button onClick={onClick}
       className={`hover:scale-105 transition relative inline-flex items-center justify-center px-4 py-2 overflow-hidden text-white bg-[#A87914] rounded-sm group cursor-pointer ${className}`}>
       <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-[#705211] rounded-full group-hover:w-56 group-hover:h-56"></span>
       <span className="relative z-10">{children}</span>
