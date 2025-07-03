deec2ad7383adf34433741c19e18cda5a0a1649b
Anik Siddiky <codesbynik@gmail.com>
2025-07-03 19:00:02 +0600

    updated the hero section and updated favicon

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -14,6 +14,16 @@ const Hero = () => {
     return (
         <div className='lg:py-36 py-10 lg:flex items-center'>
             <div className='lg:space-y-5 space-y-3 flex-1'>
+
+                <motion.h3
+                    initial={{ opacity: 0, y: -50 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{ once: false }}
+                    transition={{ duration: 0.8, ease: 'easeOut' }}
+                    className='lg:text-2xl font-medium text-center lg:text-start'>
+                    Hello there! I'm
+                </motion.h3>
+
                 <motion.h1
                     initial={{ opacity: 0, y: -50 }}
                     whileInView={{ opacity: 1, y: 0 }}
@@ -24,32 +34,28 @@ const Hero = () => {
                 </motion.h1>
 
                 <motion.h3
-                    initial={{ opacity: 0, y: -50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    viewport={{ once: false }}
-                    transition={{ duration: 0.8, ease: 'easeOut', delay: 0.5 }}
                     className='text-xl lg:text-3xl font-medium text-center lg:text-start text-gray-500'>
-                    Front-End Developer
-                </motion.h3>
-
-                <p className='lg:text-xl text-center lg:text-start md:max-w-[370px] lg:max-w-full mx-auto'>
-                    Innovative Front-End Developer crafting smooth, accessible, and visually appealing web experiences. Passionate about writing clean code that
-                    <br />
-                    <span className='text-[#A87914] font-medium'>
+                    I am a&nbsp;
+                    <span className='text-[#A87914] font-semibold'>
                         <Typewriter
                             words={[
-                                'creates intuitive and engaging interfaces!',
-                                'elevates user experience across all devices!',
-                                'transforms ideas into interactive designs!',
-                                'blends performance with aesthetic precision!',
+                                'Front-End Developer',
+                                'React Specialist',
+                                'JavaScript Enthusiast',
                             ]}
                             loop={true}
                             cursor
                             cursorStyle='|'
-                            typeSpeed={50}
+                            typeSpeed={80}
                             deleteSpeed={30}
-                            delaySpeed={1500} />
+                            delaySpeed={2000}
+                        />
                     </span>
+                </motion.h3>
+
+
+                <p className='lg:text-xl text-center lg:text-start lg:px-0 md:px-0 px-4 mx-auto'>
+                    Innovative Front-End Developer crafting smooth, accessible, and visually appealing web experiences.
                 </p>
 
                 <motion.div
